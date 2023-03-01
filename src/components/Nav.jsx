c3767dbe9186a21b74785fae22a498aef31f67b2
mannyaalonso <mannyaalonso@gmail.com>
2023-03-01 14:21:22 -0500

    remove router dom

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,6 @@
 import { Fragment } from "react"
 import { Disclosure, Menu, Transition } from "@headlessui/react"
 import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline"
-import { Link } from "react-router-dom"
 import Switcher from "./Switcher"
 
 const Nav = ({ heroRef, aboutRef, projectsRef, skillsRef, contactRef }) => {
